3df5b63da22e61d39a4d7351da0dc94adcbb9b3a
Kyle Adkins <kyle.m.adkins@gmail.com>
2023-06-30 20:16:49 -0400

    Add project files.

---
+++ FunctionalCpp/FunctionalCpp.cpp
@@ -0,0 +1,12 @@
+﻿// FunctionalCpp.cpp : Defines the entry point for the application.
+//
+
+#include "FunctionalCpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
