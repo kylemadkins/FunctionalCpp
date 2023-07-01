269452aa8ca30d6ee682b41499327a555e2c0250
Kyle Adkins <kyle.m.adkins@gmail.com>
2023-06-30 22:42:31 -0400

    Add RxCpp and Catch2 libraries

--- FunctionalCpp/FunctionalCpp.cpp
+++ FunctionalCpp/FunctionalCpp.cpp
@@ -1,6 +1,9 @@
 ﻿// FunctionalCpp.cpp : Defines the entry point for the application.
 //
 
+#include <rxcpp/rx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
 #include "FunctionalCpp.h"
 
 using namespace std;
